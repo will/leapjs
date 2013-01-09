6203bb27f7633ff64337b7bb5552f73712ba180d
Josh Hull <jhull@leapmotion.com>
2013-01-09 13:21:10 -0800

    ws

--- leap.js
+++ leap.js
@@ -21,10 +21,7 @@
       return destination; 
     }
   }
-
   var exports = window.Leap
-
-
 
 	/*	SWFObject v2.2 <http://code.google.com/p/swfobject/> 
 	is released under the MIT License <http://www.opensource.org/licenses/mit-license.php> 
