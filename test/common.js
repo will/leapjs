b5e6c02297c24328584d571ef68a0fb0a996bf16
Josh Hull <jhull@leapmotion.com>
2013-01-09 14:45:20 -0800

    add motion tests

--- test/common.js
+++ test/common.js
@@ -2,19 +2,21 @@ var fingerId = 0
   , handId = 0
   , frameId =0;
 
-fakeFrame = function(opts) {
+var fakeFrame = function(opts) {
   if (opts === undefined) opts = {};
+
   return {
     id: ++frameId,
+    valid: true,
     timestamp: frameId,
     pointables: _(opts.fingers || 0).times(function() { return fakeFinger() }),
     hands: _(opts.hands || 0).times(function() { return fakeHand() }),
-    rotation: [[0,1,2], [2,3,4], [2,3,4]],
-    translation: {x: 1, y: 2, z: 3}
+    rotation: opts.rotation || [[0,1,2], [2,3,4], [2,3,4]],
+    translation: opts.translation || {x: 1, y: 2, z: 3},
   }
 }
 
-fakeHand = function() {
+var fakeHand = function() {
   return {
     id: ++handId,
     valid: true,
@@ -24,7 +26,7 @@ fakeHand = function() {
   }
 }
 
-fakeFinger = function() {
+var fakeFinger = function() {
   return {
     id: ++fingerId,
     length: 5,
---
+++ test/motion.js
@@ -0,0 +1,32 @@
+describe('Motion', function(){
+  describe('#matrix()', function(){
+    it('should return a rotation matrix', function(){
+      var data1 = fakeFrame({rotation: [[0,1,2], [2,3,4], [2,3,4]]})
+      var frame1 = new window.Leap.Frame(data1);
+      assert.deepEqual({"elements":[[0,1,2],[2,3,4],[2,3,4]]}, frame1.matrix())
+    })
+  })
+
+  describe('#translation()', function(){
+    it('should return the translation', function(){
+      var data1 = fakeFrame({translation: {x: 1,y: 2, z:3}})
+      var frame1 = new window.Leap.Frame(data1);
+      var data2 = fakeFrame({translation: {x: 3,y: 1, z:5}})
+      var frame2 = new window.Leap.Frame(data2);
+      assert.deepEqual({x:-2, y:1, z:-2}, frame1.translation(frame2))
+    })
+  })
+
+  describe('#rotationAxis()', function(){
+    it('should return the rotationAxis', function(){
+      var data1 = fakeFrame({rotation: [[0,1,2], [2,3,4], [2,3,4]]})
+      var frame1 = new window.Leap.Frame(data1);
+      var data2 = fakeFrame({rotation: [[0,4,5], [1,3,7], [5,4,2]]})
+      var frame2 = new window.Leap.Frame(data1);
+      var result = frame1.rotationAxis(frame2);
+      assert.closeTo(-Math.sqrt(2)/2, result.x, 0.0001)
+      assert.closeTo(0, result.y, 0.0001)
+      assert.closeTo(Math.sqrt(2)/2, result.z, 0.0001)
+    })
+  })
+})
